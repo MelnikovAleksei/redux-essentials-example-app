9110388f77ff7cb120d8127227f14a6cc9b05825
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-12-07 18:12:25 +0300

    feat: add loading users

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,15 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { client } from '../../api/client';
 
-const initialState = [
-    { id: '0', name: 'Penny the spaniel' },
-    { id: '1', name: 'Leshenci' },
-    { id: '2', name: 'Mashenci' }
-]
+const initialState = [];
 
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await client.get('/fakeApi/users');
+    return response.users;
+  })
+  
 const usersSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: {
+        [fetchUsers.fulfilled]: (state, action) => {
+            return action.payload
+        }
+    }
 })
 
 export default usersSlice.reducer
